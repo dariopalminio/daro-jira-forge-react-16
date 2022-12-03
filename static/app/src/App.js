cf6fb5e35f649f4b31b6e84bcd41224fc0a25fac
Dario Palminio <dario.palminio@gmail.com>
2022-12-03 20:09:31 -0300

    Project Config for Froge Jira with react 16

---
+++ static/app/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+//import { invoke } from '@forge/bridge';
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+       //
+    }, []);
+
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>I am Daro!</p>
+        </div>
+    );
+}
+
+export default App;
