fa3aaec30ee82bbadd829d1673d4f3f332ccf588
Dario Palminio <dario.palminio@gmail.com>
2022-12-04 19:05:02 -0300

    feat: add The invoke bridge method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import Resolver from '@forge/resolver';
+//import { storage } from '@forge/api';
 
 const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
     console.log(req);
 
-    return 'Hello world!';
+    return 'The invoke bridge method enables custom UI apps to run backend FaaS functions hosted by Atlassian.';
 });
 
 export const handler = resolver.getDefinitions();
--- static/app/src/components/Hello.tsx
+++ static/app/src/components/Hello.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
+import { invoke } from '@forge/bridge';
 
 interface IProps {
     children?: React.ReactNode;
 }
 
 const Hello: React.FC<IProps> = (props: IProps) => {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const info: string = await invoke('getText', { example: 'my-invoke-variable' });
+                setData(info);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getData()
+    }, []);
 
     return (
         <div>
             <h1>Hello</h1>
             {props.children}
+            <p>Info: {data}</p>
         </div>
     );
 };
